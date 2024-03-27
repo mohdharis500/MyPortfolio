bf7e1aea2db2762de415a08a7e6005398f0d65a3
mohdharis500 <mohdharis500.mh@gmail.com>
2024-03-27 16:49:27 +0530

    add files

---
+++ src/data/constants.js
@@ -0,0 +1,274 @@
+export const Bio = {
+    name: "Mohd Haris",
+    roles: [
+      "Software Engineer",
+      "Programmar",  
+      "Front-End Web Developer",
+      "App Developer",
+        
+    ],
+    description: "Driven by a passion for learning and an insatiable appetite for challenges, I'm a versatile and motivated software engineer committed to delivering top-notch results. With a positive attitude and a growth-oriented mindset, I'm poised to make a meaningful impact and achieve extraordinary feats.",
+    github: "https://github.com/mohdharis500",
+    resume: "https://drive.google.com/file/d/1iBoA1nRLAEBxn4BESbRKRpDWwPXblvq0/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/mohd-haris-031575241?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    insta: "https://instagram.com/harryyyyyy_07?igshid=OGQ5ZDc2ODk2ZA==",
+    twitter: "https://x.com/MohdHar01484821?t=tRAlEevMB9Z1bDDfsFOtog&s=08",
+    facebook: "https://www.facebook.com/mohd.haris.5439?mibextid=ZbWKwL",
+
+};
+
+export const skills = [
+    {
+    title: "Frontend",
+    skills: [
+        {
+            name: "ReactJs",
+            image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+            name: "HTML",
+            image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+            name: "CSS",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+            name: "JavaScript",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+            name: "Bootstrap",
+            image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+            name: "Flutter",
+            image:"https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+    ],
+
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "NodeJs",
+                image: "https://nodejs.org/static/images/logo.svg",
+            },
+            {
+                name: "Python",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png",
+            },
+            {
+                name: "MySQL",
+                image: "https://d1.awsstatic.com/asset-repository/products/amazon-rds/1024px-MySQL.ff87215b43fd7292af172e2a5d9b844217262571.png",
+            },
+
+        ],
+    },
+    {
+        title: "Languages",
+        skills: [
+            {
+                name: "C",
+                image: "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+            },
+            {
+                name: "C++",
+                image: "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+            },
+            {
+                name:"Python",
+                image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png",
+            },
+            {
+                name:"Dart",
+                image: "https://dart.dev/assets/shared/dart-logo-for-shares.png?2",
+            },
+            {
+              name:"Java",
+              image: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.png",
+          },
+        ],
+    },
+    {
+        title: "Others",
+        skills: [
+            {
+                name: "Github",
+                image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            },
+            {
+                name: "VS Code",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "Chrome Developer Tools",
+                image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj2ypYi_ofhKr-yw8vkk9foiaYeZieELEBpA&usqp=CAU",
+            },
+            {
+              name: "Android Studio",
+              image:"https://cdn.icon-icons.com/icons2/3053/PNG/512/android_studio_alt_macos_bigsur_icon_190395.png",
+          },
+        ],
+    },
+];
+export const experiences = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C4D0BAQElxUMwsyu5Ig/company-logo_200_200/0/1519893420217?e=2147483647&v=beta&t=ZwuWfstIHupuOjMpGggzLJqRst9n-WPYQt_O9fXXM_0",
+      role: "Flutter Developer Intern",
+      company: "Unipro Software",
+      date: "Sep 2023 - Ongoing",
+      desc: "Learning to develop apps for different platforms using Flutter",
+      skills: [
+        "Dart",
+        "Flutter",
+        "Android Studio",
+        "WordPress",
+        
+      ],
+      doc: "",
+    },
+    {
+        id: 1,
+        img: "https://media.licdn.com/dms/image/D560BAQFV0JYJ33yONg/company-logo_200_200/0/1691701551582?e=2147483647&v=beta&t=QkrAq-RgRFTB6KVkKq2C_E7toRJXL27_M-cVoR_smSk",
+        role: "Web Developer Intern",
+        company: "Bharat Intern",
+        date: "Aug 2023 - Sep 2023",
+        desc: "Created various websites like portfolio website, temperature converter, etc.",
+        skills: [
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "VS Code",
+          "Chrome Developer Tools"
+          
+        ],
+        doc: "",
+      },
+];
+
+export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjyVmcQX4nMbfYhCPR1Ko4zjWo5bnoGl9DMbbQINrQn0GKHSNNAOdm1inf2E2usgNH15o&usqp=CAU",
+      school: "Rajkiya Engineering College , Ambedkar Nagar",
+      date: "2020 - 2024",
+      grade: "8.1 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Information Technology at Rajkiya Engineering College, Ambedkar Nagar. I have completed 6 semesters and have a CGPA of 8.1. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Information Technology",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDMf06KM2dHpHcqYT5IE8lpHSyjtktY81BGcpZiSrNVOHiCti3fitqk2kvN15xr16HDM&usqp=CAU",
+      school: "Sayyid Hamid Senior Secondary School, Aligarh",
+      date: "2019",
+      grade: "83.6%",
+      desc: "I completed my class 12 education at Sayyid Hamid Senior Secondary School, Aligarh, AMU where I studied Science with Mathematics.",
+      degree: "AMU(XII)",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFYqyTcbmAMMQqM8s_31ykFQf2iwbj6c3c_Q&usqp=CAU",
+      school: "DAV Public School",
+      date: "2017",
+      grade: "9.8 CGPA",
+      desc: "I completed my class 10 education at DAV Public School, Tanda",
+      degree: "CBSE(X)",
+    },
+  ];
+  export const projects = [
+    {
+      id: 9,
+      title: "Weather App",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "WeatherNow is a real-time weather application that provides up-to-date weather information for any location worldwide. With a sleek and intuitive user interface, users can quickly access essential weather data with just a few clicks.",
+      image:
+        "https://drive.google.com/file/d/1EXkJc8EanTu4HRv9j587ck6PAbCjtzTM/view?usp=sharing",
+      tags: [
+        "React",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "API Call",
+        "Material UI"
+      ],
+      category: "web app",
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 5,
+      title: "Todo App",
+      date: "Aug 2023",
+      description:
+        " A Todo App made with Flutter and Dart. It is a minimalist task management application designed to help users organize their daily activities efficiently. With a clean and intuitive user interface, this app provides a straightforward solution for creating, editing, and managing tasks on-the-go.",
+      image:
+        "",
+      tags: ["Dart" , "Flutter" , "Android Studio", "Visual Studio Code",],
+      category: "web app",
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 6,
+      title: "Portfolio Website",
+      date: "Oct 2023",
+      description:
+        "The Personal Portfolio Website is a showcase of my skills, projects, and experiences in the field of Software Engineering. Designed with a clean and modern aesthetic, this website serves as a platform to highlight my achievements and provide visitors with an insight into my background and capabilities.",
+      image:
+        "",
+      tags: ["React Js", "HTML", "CSS", "JavaScript", "Material UI", "EmailJS", "Github", ],
+      category: "web app",
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 8,
+      title: "Temperature Converter",
+      date: "May 2022",
+      description:
+        "The Simple Temperature Converter is a straightforward utility designed to assist users in converting temperatures between different units. With a clean and intuitive user interface, this web-based tool provides a quick and hassle-free solution for converting temperatures on the fly.",
+      image:
+        "",
+      tags: ["HTML", "CSS", "JavaScript", "VS Code"],
+      category: "web app",
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 7,
+      title: "Emotion Detection",
+      date: "Oct 2022 - Feb 2023",
+      description:
+        "The Emotion Detection project is an advanced machine learning application designed to analyze facial expressions and accurately identify the emotions conveyed by a person. Leveraging the power of artificial intelligence, this project has the potential to revolutionize fields such as sentiment analysis, human-computer interaction, and mental health assessment.",
+      image:
+        "",
+      tags: [
+        "Machine Learning",
+        "Deep Learning",
+        "Computer Vision",
+        "Emotion Recognition",
+        "TensorFlow",
+        "Keras",
+        "Python",
+        "OpenCV",
+        "Data Preprocessing",
+        "Artificial Intelligence",
+      ],
+      category: "machine learning",
+      github: "",
+      webapp: "",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
+  
